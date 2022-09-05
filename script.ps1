74e0e5a7e1bea4d67689d03a0af6a20472f97728
Oliver Cutting <oliver.cutting@engineering.digital.dwp.gov.uk>
2022-09-05 13:24:02 +0100

    Set firefox as default browser

--- script.ps1
+++ script.ps1
@@ -19,10 +19,23 @@ namespace Win32{
 add-type $code 
 [Win32.Wallpaper]::SetWallpaper($MyWallpaper)
 
+# INSTALL FIREFOX
+Invoke-WebRequest -Uri "https://download.mozilla.org/?product=firefox-latest-ssl&os=win64&lang=en-GB" -OutFile "C:\Users\olive\Downloads\firefoxsetup.exe"
+Start-Process -FilePath "firefoxsetup.exe" -WorkingDirectory "C:\Users\olive\Downloads\"
+
+# SET FIREFOX AS DEFAULT BROWSER
+$regKey      = "HKCU:\Software\Microsoft\Windows\Shell\Associations\UrlAssociations\{0}\UserChoice"
+$regKeyFtp   = $regKey -f 'ftp'
+$regKeyHttp  = $regKey -f 'http'
+$regKeyHttps = $regKey -f 'https'
+Set-ItemProperty $regKeyFtp   -name ProgId FirefoxURL
+Set-ItemProperty $regKeyHttp  -name ProgId FirefoxURL
+Set-ItemProperty $regKeyHttps -name ProgId FirefoxURL
+
 # INSTALL STEAM
-Invoke-WebRequest -Uri "https://cdn.cloudflare.steamstatic.com/client/installer/SteamSetup.exe" -OutFile "C:\Users\olive\Downloads\SteamSetup.exe"
-Start-Process -FilePath "SteamSetup.exe" -WorkingDirectory "C:\Users\olive\Downloads\"
+# Invoke-WebRequest -Uri "https://cdn.cloudflare.steamstatic.com/client/installer/SteamSetup.exe" -OutFile "C:\Users\olive\Downloads\SteamSetup.exe"
+# Start-Process -FilePath "SteamSetup.exe" -WorkingDirectory "C:\Users\olive\Downloads\"
 
 # INSTALL BATTLE.NET
-
+# Invoke-WebRequest -Uri "https://cdn.cloudflare.steamstatic.com/client/installer/SteamSetup.exe" -OutFile "C:\Users\olive\Downloads\Battle.net-Setup.exe"
 # Start-Process -FilePath "Battle.net-Setup.exe" -WorkingDirectory "C:\Users\olive\Downloads\"
