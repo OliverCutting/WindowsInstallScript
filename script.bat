896043305108f57f6a539a81cfedcea6f8ff93aa
Oliver Cutting <oliver.cutting@engineering.digital.dwp.gov.uk>
2022-09-05 11:40:17 +0100

    Added Powershell wallpaper changer

--- script.bat
+++ script.bat
@@ -1,6 +1,6 @@
 REM Set desktop background
 MOVE C:\Users\olive\Downloads\mountains2.jpg C:\Users\olive\Pictures\mountains2.jpg
-START C:\Users\olive\Projects\WindowsInstallScript\wallpaper
+PowerShell -File C:\Users\olive\Projects\WindowsInstallScript\wallpaper.ps1
 
 REM Start installers
 START C:\Users\olive\Downloads\SteamSetup
