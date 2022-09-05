4a5bf7cca430596680541a8dda73f41e481b26f7
Oliver Cutting <oliver.cutting@engineering.digital.dwp.gov.uk>
2022-09-05 11:56:42 +0100

    Started Powershell script

--- script.bat
+++
@@ -1,7 +0,0 @@
-REM Set desktop background
-MOVE C:\Users\olive\Downloads\mountains2.jpg C:\Users\olive\Pictures\mountains2.jpg
-PowerShell -File C:\Users\olive\Projects\WindowsInstallScript\wallpaper.ps1
-
-REM Start installers
-START C:\Users\olive\Downloads\SteamSetup
-START C:\Users\olive\Downloads\Battle.net-Setup
---
+++ script.ps1
@@ -0,0 +1,28 @@
+# SET WALLPAPER
+Move-Item -Path C:\Users\olive\Downloads\mountains2.jpg -Destination C:\Users\olive\Pictures\mountains2.jpg
+$MyWallpaper="C:\Users\olive\Pictures\mountains2.jpg"
+$code = @' 
+using System.Runtime.InteropServices; 
+namespace Win32{ 
+    
+     public class Wallpaper{ 
+        [DllImport("user32.dll", CharSet=CharSet.Auto)] 
+         static extern int SystemParametersInfo (int uAction , int uParam , string lpvParam , int fuWinIni) ; 
+         
+         public static void SetWallpaper(string thePath){ 
+            SystemParametersInfo(20,0,thePath,3); 
+         }
+    }
+ } 
+'@
+
+add-type $code 
+[Win32.Wallpaper]::SetWallpaper($MyWallpaper)
+
+# INSTALL STEAM
+Invoke-WebRequest -Uri "https://cdn.cloudflare.steamstatic.com/client/installer/SteamSetup.exe" -OutFile "C:\Users\olive\Downloads\"
+Start-Process -FilePath "SteamSetup.exe" -WorkingDirectory "C:\Users\olive\Downloads\"
+
+# INSTALL BATTLE.NET
+
+# Start-Process -FilePath "Battle.net-Setup.exe" -WorkingDirectory "C:\Users\olive\Downloads\"
