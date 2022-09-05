c997923197fcd51b07817fa897877fceb8890eb1
Oliver Cutting <oliver.cutting@engineering.digital.dwp.gov.uk>
2022-09-05 11:15:18 +0100

    Added VBS wallpaper changer

--- script.bat
+++ script.bat
@@ -1,5 +1,5 @@
 REM Set desktop background
 MOVE C:\Users\olive\Downloads\mountains2 C:\Users\olive\Pictures\mountains2
-START C:\Users\olive\Projects\WindowsInstallScript\wallpaper.vbs
+START C:\Users\olive\Projects\WindowsInstallScript\wallpaper
 START C:\Users\olive\Downloads\SteamSetup
 START C:\Users\olive\Downloads\Battle.net-Setup
